f97cac61a18afc2b796cdc12d0ffb19bb8dad3ff
Vinicius-1DS <vinicius.demarque15@gmail.com>
2024-09-08 19:25:44 -0300

    Update TipoDespesa.java

--- Sistema de Controle de Despesas/TipoDespesa.java
+++ Sistema de Controle de Despesas/TipoDespesa.java
@@ -1,23 +1,23 @@
 import java.io.Serializable;
 import java.util.ArrayList;
-
+//inserindo a classe tipo despesas.
 public class TipoDespesa implements Serializable {
     private ArrayList<String> tiposDespesa;
-
+    //Criando a lista com os tipos de despesas inseridas pelo ususario.
     public TipoDespesa() {
         tiposDespesa = new ArrayList<>();
     }
-
+    //Adicionando tipo de despesa.
     public void adicionarTipo(String tipo) {
         tiposDespesa.add(tipo);
     }
-
+    //Listandop tipos de despesas ja cadastradas.
     public void listarTipos() {
         for (String tipo : tiposDespesa) {
             System.out.println(tipo);
         }
     }
-
+    //Removendo tipos de despesas disponiveis.
     public void removerTipo(String tipo) {
         tiposDespesa.remove(tipo);
     }
